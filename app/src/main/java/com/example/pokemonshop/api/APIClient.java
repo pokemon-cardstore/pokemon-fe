90f5ebba70abdc3f35459f8a59f1c81f5ccd465b
kiet <trangiakit1010@gmail.com>
2025-06-28 19:25:01 +0700

    login basic layout product

--- app/src/main/java/com/example/pokemonshop/api/APIClient.java
+++ app/src/main/java/com/example/pokemonshop/api/APIClient.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
-    private static String baseURL = "http://192.168.1.76:5275/api/v1/";
+    private static String baseURL = "http://10.0.2.2:5275/api/v1/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
