86c372dc5399f17bc10fc043d365c064b8e88181
nam nguyễn <nhatnamgt7@gmail.com>
2025-07-13 21:49:17 +0700

    register success but link active still bug

--- app/src/main/java/com/example/pokemonshop/api/auth/AuthService.java
+++ app/src/main/java/com/example/pokemonshop/api/auth/AuthService.java
@@ -5,16 +5,23 @@ import com.example.pokemonshop.model.LoginResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
+
+import com.example.pokemonshop.model.RegisterDto;
 import com.google.gson.JsonObject;
 
 public interface AuthService {
     @POST("Auth")
     Call<LoginResponse> login(@Query("email") String email, @Query("password") String password);
 
-    @POST("Customer")
-    Call<Void> register(@Body Customer customer);
+    @POST("Register")
+    Call<Void> register(@Body RegisterDto dto);
+
+    @GET("Activate")
+    Call<String> activateAccount(@Query("email") String email);
+
 
     @POST("auth/google-login")
     Call<LoginResponse> loginWithGoogle(@Body JsonObject idTokenBody);
