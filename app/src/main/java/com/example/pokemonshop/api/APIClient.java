dbb81562db67cea9c2578bad077223a534f3d52b
conboaduide <vuvananhben26@gmail.com>
2025-07-21 08:11:25 +0700

    push docker hub

--- app/src/main/java/com/example/pokemonshop/api/APIClient.java
+++ app/src/main/java/com/example/pokemonshop/api/APIClient.java
@@ -12,8 +12,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
     // private static String baseURL = "http://10.0.2.2:5275/api/v1/";
-
-      private static String baseURL = "http://10.0.2.2:8080/api/v1/";
+    //"ReturnUrl": "http://10.0.2.2:8080/api/Payment/vnpay-return",
+      //private static String baseURL = "http://10.0.2.2:8080/api/v1/";
+      private static String baseURL = "https://korrosql.zapto.org/api/v1/";
 
     private static Retrofit retrofit = null;
 
